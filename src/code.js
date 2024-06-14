9dcfbd0d28187158c7961420587c8b78ace1b802
Dzianis <somniummisellus@gmail.com>
2024-06-14 20:32:38 +0300

    rjomba-bomba

--- src/code.js
+++ src/code.js
@@ -3,11 +3,11 @@ function onClickHandler(e) {
     sum += 1
     localStorage.setItem('sum', '' + sum)
     let counter = document.getElementById('counter');
-    counter.innerText = sum.toString();
+    counter.innerText = sum.toString() + ' $RJMB';
 }
 
 
 let sum = Number(localStorage.getItem('sum'));
 let counter = document.getElementById('counter');
-counter.innerText = sum.toString();
+counter.innerText = sum.toString() + ' $RJMB';
 document.getElementById('Rjomba').addEventListener('click', e => onClickHandler(e))
