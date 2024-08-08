591af0b9915399f344c5cbe47a743b88dd1717b7
Dzianis <somniummisellus@gmail.com>
2024-08-08 22:52:53 +0300

    rjomba-bomba

--- src/test/ad.js
+++ src/test/ad.js
@@ -1,10 +1,11 @@
 window.googletag = window.googletag || {cmd: []};
 googletag.cmd.push(function() {
-    googletag.defineSlot('/21857590943,22657440065/tgads_test/300x250', [[300, 300], [300, 250]], 'banner').addService(googletag.pubads());
-    googletag.pubads().set("page_url", "taraftar.tv");
+    googletag.defineSlot('/21857590943,22960671442/tgads_test/300x250', [[300, 300], [300, 250]], 'banner').addService(googletag.pubads());
+    googletag.pubads().set("page_url", "playhop.com");
     googletag.pubads().enableSingleRequest();
     googletag.pubads().collapseEmptyDivs();
     googletag.enableServices();
 });
+
 
 googletag.cmd.push(function() { googletag.display('banner'); });
